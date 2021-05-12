f2bfa4dc963247e85610c55b8393c35867fd3a42
BrenKenna <55492113+BrenKenna@users.noreply.github.com>
2021-05-12 16:31:17 +0100

    Update manager.py
    Added methods to: i). change active dynamo_table object.  ii). Monitor task table over N iterations and Y seconds between iterts. iii). Update list of itemIDs to desired itemState. iv). Auto-handling of the dynamo_table object given user defined table_name

--- code/manager.py
+++ code/manager.py
@@ -16,10 +16,11 @@ class PyAnamo_Manager(pc.PyAnamo_Client):
 		The PyAnamo_Manager includes methods to:
 			# Create / Remove workflow table (Manager)
 			# Describe the workflow table schema (Manager)
+			- Set dynamo_table property
 			- Alter table provisioning and set auto-scaling (Manager)
 			- Check whether specific items do or do not exist (Manager + Client-getCurrentState)
 			- Import task, list or text file of tasks to table (Manager)
-			- Summarize the PyAnamo item states (optionally over-time) (Manager + Client-itemCounter)
+			# Summarize the PyAnamo item states (optionally over-time) (Manager + Client-itemCounter)
 			- Change item states (optionally user-defined string (Manager + Client)
 			- Retrieve item logs, instanceIDs (Manager + Client-getToDoItems)
 			- Translate itemStates to AWS-Batch job states (Manager + Client)
@@ -38,6 +39,37 @@ class PyAnamo_Manager(pc.PyAnamo_Client):
 
 		elif type(dynamo_table) is not str:
 			self.dynamo_table = dynamo_table
+
+
+	# Set dynamoDB table proberty to self
+	def set_dynamoProperty(self, table_name):
+
+		# Set property and log to user
+		try:
+			self.dynamo_table = self.dynamodb.Table(table_name)
+			check = self.dynamo_table.__dict__['_name']
+			out = str('Dynamo Table Object now = ' + check)
+		except:
+			out = str('Error setting table = ' + table_name + ', please check that it exists')
+		return(out)
+
+
+	# Handle dynamo_table object
+	def handle_DynamoTable(self, table_name):
+
+		# Check table object is a property
+		if 'dynamo_table' in self.__dict__:
+
+			# Only update if table names do not match
+			currentTable = self.dynamo_table.__dict__['_name']
+			if currentTable != table_name:
+				out = self.set_dynamoProperty(table_name)
+				return(out)
+
+		# Otherwise set
+		else:
+			out = self.set_dynamoProperty(table_name)
+			return(out)
 
 
 	# Check if table exists
@@ -123,6 +155,9 @@ class PyAnamo_Manager(pc.PyAnamo_Client):
 	# Delete workflow table
 	def delete_workflow_table(self, table_name):
 
+		# Handle table object
+		handle_DynamoTable(self, table_name)
+
 		# Delete table if exists
 		table_exists = self.check_table(table_name)
 		if table_exists == 1:
@@ -137,3 +172,60 @@ class PyAnamo_Manager(pc.PyAnamo_Client):
 			out = int(0)
 			print('Table = ' + table_name + ' does not exist')
 		return(out)
+
+
+	# Monitor tasks
+	def monitor_task(self, table_name, Niterations = 1, waitTime = 0):
+
+		# Handle table object
+		handle_DynamoTable(self, table_name)
+
+		# Run item counter for N iterations, with wait time between them		
+		N = 0
+		while N < Niterations:
+
+			# Get item IDs per state
+			N += 1
+			taskSummary = {}
+			itemData = self.itemCounter(self.dynamo_table)
+
+			# Populate taskSummary
+			if itemData['ItemCount'] > 0:
+				for itemState in [ 'todo', 'locked', 'done' ]:
+					stateN = len(itemData[itemState])
+					taskSummary.update( { itemState: int(stateN) } )
+
+				# Wait and return results
+				time.sleep(int(waitTime))
+				return(taskSummary)
+
+			# Otherwise log
+			else:
+				print('No items retrived from table = ' + table_name)
+
+
+	# Set itemStates
+	def updateItemStates(self, table_name, itemID_list, itemState):
+
+		# Iteratively update the list of items to itemState
+		handle_DynamoTable(self, table_name)
+		counter = 0
+		failedItems = []
+		for itemID in itemID_list:
+
+			# Change state
+			try:
+				response = self.dynamo_table.update_item(
+					Key = {'itemID': itemID},
+					ExpressionAttributeNames = { "#state": "ItemState" },
+					ExpressionAttributeValues = { ":itemstate": str(itemState) },
+					UpdateExpression = "SET #state = :itemstate",
+					ReturnValues="UPDATED_NEW"
+				)
+				counter += 1
+			except:
+				failedItems.append(itemID)
+
+		# Log status
+		print('Successfully update N = ' + str(counter) + " items to state = " + itemState + " and failed to update N = " + str(len(failedItems)) + " items")
+		return(failedItems)
