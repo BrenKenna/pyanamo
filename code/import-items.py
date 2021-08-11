b35da89f3928fbc2fd8d3fbd0a65210cf4433d07
BrenKenna <55492113+BrenKenna@users.noreply.github.com>
2021-08-11 11:04:17 +0200

    Update import-items.py
    Added argparse to the item import wrapper

--- code/import-items.py
+++ code/import-items.py
@@ -2,23 +2,74 @@
 
 
 # Import modules
+import argparse
 import sys, json, time
 import manager as pmanager
 
 
-# Parse input
-user_data = {
-	"table_name": sys.argv[1],
-	"aws_region": sys.argv[2],
-	'data': sys.argv[3],
-	'delim': sys.argv[4],
-	'nested_delim': None
-}
+################################################
+################################################
+# 
+# Handle Arguments
+# 
+################################################
+################################################
 
 
-# Add nested delimiter if provided
-if len(sys.argv) > 5:
-	user_data['nested_delim'] = sys.argv[5]
+# Parse arguments
+parser = argparse.ArgumentParser(description = """Import a list of items into the supplied DynamoDB table from a file. The specified DynamoDB table is created if it does not exists. The input file must adhere to the standardized PyAnamo schema. The format for the input file is shown below and must include the header. The field delimiter must be provided, whereas the nested task delimiter is an optional argument.
+
+Example Input Data Format:
+
+	itemID|TaskID|TaskScript|TaskArgs
+	SRR6776829_TARDBP_FUS|SRR6776829|bash ${PIPELINE}/HaplotypeCaller.sh SRR6776829|chr1:11012344-11030528,chr16:31180138-31191605
+
+	Field Delimiter = '|'
+	OPTIONAL Nested task delimiter = ','
+""", formatter_class = argparse.RawTextHelpFormatter)
+
+
+# Arguments for code
+parser.add_argument("-t", "--table_name", action = 'store', type = str, help = "DynamoDB table to query\n")
+parser.add_argument("-i", "--input_data", action = 'store', type = str, help = "Text file containing the items to import into the supplied DynamoDB Table\n")
+parser.add_argument("-d", "--field_delimiter", action = 'store', type = str, help = "The field delimiter of the input data \n")
+parser.add_argument("-r", "--aws_region", action = 'store', type = str, help = "AWS region that the dynamo table is in\n")
+parser.add_argument("-n", "--nested_delimiter", action = 'store', type = str, help = "Optional Argument: Delimiter separating the nested task arguments\n")
+args = parser.parse_args()
+
+
+# Exit if key argument are not supplied
+if args.table_name == None or args.input_data == None or args.field_delimiter == None or args.aws_region == None:
+
+	# Exit if no table is provided
+	sys.stdout.write('\n\nExiting, key argumets not provided. Required arguments are DynamoDB Table Name, a file, field delimiter of file and AWS Region of the DynamoDB Table\n')
+	parser.print_help()
+	sys.exit()
+
+
+# Otherwise proceed to parsing arguments
+else:
+	user_data = {
+		'table_name': args.table_name,
+		'aws_region': args.aws_region,
+		'data': args.input_data,
+		'delim': args.field_delimiter,
+		'nested_delim': None
+	}
+
+# Handle optional arguments
+if args.parallel_items != None:
+	user_data['nested_delim'] = args.nested_delimiter
+
+del parser, argparse, args
+
+##################################################
+##################################################
+# 
+# Import items into table
+# 
+##################################################
+##################################################
 
 
 # Check table
